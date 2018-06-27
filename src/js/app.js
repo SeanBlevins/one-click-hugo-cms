12d92e264381adc50a999302ebf9aa0b34a04dd3
Sean Blevins <sean.blevins@kussbus.lu>
2018-06-27 20:30:51 +0200

    photoswipe gallery for portfolio init commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,16 @@
 // JS Goes here - ES6 supported
+import PhotoSwipe from "photoswipe";
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
+import { initPhotoSwipeFromDOM } from "./initPhotoSwipeFromDOM"
+
+console.log(PhotoSwipeUI_Default);
+
+window.PhotoSwipe = PhotoSwipe;
+window.PhotoSwipeUI_Default = PhotoSwipeUI_Default;
+
+initPhotoSwipeFromDOM('.my-gallery');
+
+
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
